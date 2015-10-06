5e34b216ee698ff8068a3efe371bd96f7337aa41
Flávio Zavan <flaviozavan@gmail.com>
2015-10-05 21:04:50 -0300

    Wrap around image list

--- commands.c
+++ commands.c
@@ -270,11 +270,9 @@ bool ci_navigate(arg_t n)
 {
 	if (prefix > 0)
 		n *= prefix;
-	n += fileidx;
-	if (n < 0)
-		n = 0;
-	if (n >= filecnt)
-		n = filecnt - 1;
+	n = (n+fileidx) % filecnt;
+        if (n < 0)
+		n += filecnt;
 
 	if (n != fileidx) {
 		load_image(n);
